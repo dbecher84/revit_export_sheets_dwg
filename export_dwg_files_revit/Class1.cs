a625d54c1333a6e4e8eed36b067112d6773e41b9
dbecher84 <dbecher84@gmail.com>
2020-06-18 13:50:53 -0400

    Initial upload

---
+++ export_dwg_files_revit/Class1.cs
@@ -0,0 +1,97 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Drawing;
+
+namespace export_sheets_dwg_revit
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            //Get application and documnet objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            //DWG Settings export settings
+            DWGExportOptions opt = new DWGExportOptions
+            {
+                HideScopeBox = true,
+                HideReferencePlane = true,
+                SharedCoords = false,
+                MergedViews = true,
+                NonplotSuffix = "NPL",
+                ExportingAreas = false,
+                FileVersion = ACADVersion.R2013,
+            };
+
+            //collect sheets in revit
+            FilteredElementCollector viewCollector = new FilteredElementCollector(doc);
+            viewCollector.OfClass(typeof(ViewSheet));
+
+            //list of sheets to send to the windows form viewList
+            List<string> listSheetNames = new List<string>();
+            foreach (ViewSheet view in viewCollector)
+            {
+                listSheetNames.Add(view.Name);
+            }
+
+
+
+            //initate list of sheets form
+            var l = new Export_Sheets.viewList(listSheetNames);
+            l.ShowDialog();
+
+            //select save folder
+            string folderPath = "";
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+            }
+
+            //list of sheets exported
+            List<string> Sheetlist = new List<string>();
+    
+
+            //check if revit sheet is in list and export to DWG if it is
+            foreach (ViewSheet view in viewCollector)
+            {
+                foreach (var item in l.ExportViewList)
+                    if (view.Name == item)
+                    {
+                        List<ElementId> SheetlistID = new List<ElementId>();
+                        SheetlistID.Add(view.Id);
+                        Sheetlist.Add(view.Name);
+                        doc.Export(folderPath, view.Name + ".dwg", SheetlistID,  opt);
+                    }
+            }
+
+            //Combine all view into string to show user what was exported
+            string fullList = "";
+            for (int i = 0; i < Sheetlist.Count; i++)
+            {
+                if (i < Sheetlist.Count - 1)
+                    fullList = fullList + Sheetlist[i] + ", ";
+                else
+                    fullList += Sheetlist[i];
+            }
+            //Display list of exported sheets
+            TaskDialog.Show("The following view were exported.", fullList);
+
+            return Result.Succeeded;
+
+        }
+    }
+}
---
+++ export_dwg_files_revit/viewList.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Form = System.Windows.Forms.Form;
+
+namespace Export_Sheets
+{
+    public partial class viewList : Form
+    {
+        List<string> fullList = new List<string>();
+
+        public viewList(List<string> list)
+        {
+            InitializeComponent();
+
+            foreach (string item in list)
+            {
+                //MessageBox.Show(item);
+                listBox1.Items.Add(item);
+                fullList.Add(item);
+            }
+        }
+
+        public List<string> ExportViewList { get; set; }
+
+        //create view list to send for export
+
+        public List<string> CreateViewList()
+        {
+            List<string> viewListWorking = new List<string>();
+            if (listBox2.Items != null)
+            {
+                foreach (var item in listBox2.Items)
+                {
+                    string text = item.ToString();
+                    //MessageBox.Show(text);
+                    viewListWorking.Add(text);
+                }
+                ExportViewList = viewListWorking;
+                return ExportViewList;
+            }
+            else
+            {
+                MessageBox.Show("List Box was empty");
+                return ExportViewList;
+            }
+        }
+
+      
+        //add item to second list on double click
+        private void DoubleClick1(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                listBox2.Items.Add(listBox1.SelectedItem);
+            }
+        }
+
+        //remove item from second list on double click
+        private void DoubleClick2(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                listBox2.Items.Remove(listBox2.SelectedItem);
+            }
+        }
+
+        //clear list 2
+        private void ClearAll(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+        }
+
+        //add all view to list 2
+        private void AddAll(object sender, EventArgs e)
+        {
+            foreach (var item in listBox1.Items)
+            {
+                listBox2.Items.Add(item);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //add all 3d views to list for export
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateViewList();
+        }
+
+        //view list to filter list box 1 
+        private void Filter_Load(object sender, EventArgs e)
+        {
+            string uText = textBox1.Text;
+            string cText = uText.Trim();
+            //MessageBox.Show(cText);
+            List<string> filteredList = new List<string>();
+            foreach (string item in listBox1.Items)
+            {
+                if (item.Contains(cText))
+                {
+                    filteredList.Add(item);
+                }
+            }
+            listBox1.Items.Clear();
+            foreach (string item in filteredList)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            foreach (string item in fullList)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+    }
+}
