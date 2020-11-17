6afadc75b65321c83aa5aa8523df6d44746c85b1
dbecher84 <dbecher84@gmail.com>
2020-11-17 15:28:37 -0500

    created 2020 toolbar

---
+++ 2020 Revit/export_dwg_files_revit/ExportedList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Export_Sheets_Final
+{
+    public partial class ExportedList : Form
+    {
+        public ExportedList(List<string> list)
+        {
+            InitializeComponent();
+
+            foreach (string item in list)
+            {
+                //MessageBox.Show(item);
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+     
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExportedList_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 2020 Revit/export_dwg_files_revit/exportSheets.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System.Drawing;
+
+namespace export_sheets_dwg
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class exportSheets : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            //Get application and documnet objects
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            //DWG Settings export settings
+            DWGExportOptions opt = new DWGExportOptions
+            {
+                HideScopeBox = true,
+                HideReferencePlane = true,
+                SharedCoords = false,
+                MergedViews = true,
+                NonplotSuffix = "NPL",
+                ExportingAreas = false,
+                FileVersion = ACADVersion.R2013,
+            };
+
+            //collect sheets in revit
+            FilteredElementCollector viewCollector = new FilteredElementCollector(doc);
+            viewCollector.OfClass(typeof(ViewSheet));
+
+            //list of sheets to send to the windows form viewList
+            List<string> listSheetNums = new List<string>();
+            foreach (ViewSheet view in viewCollector)
+            {
+                listSheetNums.Add(view.SheetNumber);
+            }
+
+
+
+            //initate list of sheets form
+            var l = new Export_Sheets.sheetList(listSheetNums);
+            l.ShowDialog();
+
+            //select save folder
+            string folderPath = "";
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+            }
+
+            //list of sheets exported
+            List<string> Sheetlist = new List<string>();
+    
+
+            //check if revit sheet is in list and export to DWG if it is
+            foreach (ViewSheet view in viewCollector)
+            {
+                foreach (var item in l.ExportSheetList)
+                    if (view.SheetNumber == item)
+                    {
+                        List<ElementId> SheetlistID = new List<ElementId>();
+                        SheetlistID.Add(view.Id);
+                        Sheetlist.Add(view.SheetNumber);
+                        doc.Export(folderPath, view.SheetNumber + ".dwg", SheetlistID,  opt);
+                    }
+            }
+
+            //Display form listing sheets that were exported
+            var d = new Export_Sheets_Final.ExportedList(Sheetlist);
+            d.ShowDialog();
+
+            return Result.Succeeded;
+
+        }
+    }
+}
